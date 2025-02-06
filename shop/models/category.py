727a55662b0129ebdc844de118a771e72524839b
pablo <pabloescm@hotmail.com>
2025-02-07 09:55:12 +1300

    Cambiando campo imagen del model category de file a url

--- shop/models/category.py
+++ shop/models/category.py
@@ -4,7 +4,8 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+    
 
     def __str__(self):
         return self.name
