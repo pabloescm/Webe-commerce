690d734439ef75f1ce5accc9da78ee133e310c20
pablo <pabloescm@hotmail.com>
2025-02-12 05:51:28 +1300

    agregando los import en admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models.category import Category
+from .models.product import Product
+from .models.product_image import ProductImage
 
 admin.site.register(Category)
 admin.site.register(Product)
