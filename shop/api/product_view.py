31bb8817fc75ffb10af3df9d0c9bfa0295dfd456
pablo <pabloescm@hotmail.com>
2025-02-12 04:51:39 +1300

    agregando producto por categoria

--- shop/api/product_view.py
+++ shop/api/product_view.py
@@ -5,3 +5,9 @@ from shop.serializers import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    @action(detail=True, methods=['get'])
+    def by_category(self, request, pk=None):
+        products = Product.objects.filter(category_id=pk)
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
