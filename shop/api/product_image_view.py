efe135d11e2cd8c8cde50888c2787603e8fc2eda
pablo <pabloescm@hotmail.com>
2025-02-21 16:31:13 +1300

    Agregando ruta imágenes por productos

--- shop/api/product_image_view.py
+++ shop/api/product_image_view.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from shop.models.product_image import ProductImage
 from shop.serializers import ProductImageSerializer
 
 class ProductImageViewSet(viewsets.ModelViewSet):
     queryset = ProductImage.objects.all()
     serializer_class = ProductImageSerializer
+
+    @action(detail=True, methods=['get'], url_path='images')
+    def get_images(self, request, pk=None):
+        images = ProductImage.objects.filter(product_id=pk)
+        serializer = self.get_serializer(images, many=True)
+        return Response(serializer.data)
--- shop/models/product.py
+++ shop/models/product.py
@@ -7,7 +7,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
-    image_url = models.URLField(max_length=200, blank=True, null=True)  # Campo para la imagen principal
 
     def __str__(self):
-        return self.name
+        return self.nam
